75ce4db88587fe2a5420198e3e395586356869c9
Takuya Nakanishi <nemumonz@yahoo.co.jp>
2017-09-15 10:47:16 +0900

    リスナー追加、ボタンタッチ音OFF
    リスナー追加、ボタンタッチ音OFF

--- KotlinBox/app/src/main/java/jp/gr/java_conf/testa/kotlinbox/MainActivity.kt
+++ KotlinBox/app/src/main/java/jp/gr/java_conf/testa/kotlinbox/MainActivity.kt
@@ -3,7 +3,10 @@ package jp.gr.java_conf.testa.kotlinbox
 import android.app.Activity
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+
+val LOGTAG = "MainActivity"
 
 class MainActivity : Activity() {
 
@@ -12,6 +15,42 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        txt_title.setText(title)
+        setListener()
+    }
+
+    /** リスナー登録 */
+    fun setListener() {
+        input_reset.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_reset)) }
+        input_sharp.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_sharp)) }
+        input_flat.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_flat)) }
+        input_minor.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_minor)) }
+
+        input_c.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_c)) }
+        input_d.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_d)) }
+        input_e.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_e)) }
+        input_f.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_f)) }
+        input_g.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_g)) }
+        input_a.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_a)) }
+        input_b.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_b)) }
+
+        input_6.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_6)) }
+        input_7.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_7)) }
+        input_maj7.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_maj7)) }
+        input_harfdim.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_harfdim)) }
+        input_dim.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_dim)) }
+        input_dim_maj7.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_dim_maj7)) }
+        input_aug.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_aug)) }
+        input_aug_maj7.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_aug_maj7)) }
+        input_7sus4.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_7sus4)) }
+
+        input_flat5.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_flat5)) }
+        input_sharp5.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_sharp5)) }
+        input_flat9.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_flat9)) }
+        input_9.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_9)) }
+        input_sharp9.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_sharp9)) }
+        input_11.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_11)) }
+        input_sharp11.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_sharp11)) }
+        input_13.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_13)) }
+        input_flat13.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_flat13)) }
     }
 }
