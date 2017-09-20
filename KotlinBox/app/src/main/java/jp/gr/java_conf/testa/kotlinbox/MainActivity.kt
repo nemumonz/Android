fed5bc3553ae7a025e8e56cbceb021fc3a0e13fc
Takuya Nakanishi <nemumonz@yahoo.co.jp>
2017-09-20 13:57:11 +0900

    TextからImageを生成
    ・TextからImageを生成
    ・×を押すとTextがくるくるまわるアニメーション追加

--- KotlinBox/app/src/main/java/jp/gr/java_conf/testa/kotlinbox/MainActivity.kt
+++ KotlinBox/app/src/main/java/jp/gr/java_conf/testa/kotlinbox/MainActivity.kt
@@ -1,26 +1,38 @@
 package jp.gr.java_conf.testa.kotlinbox
 
+import android.animation.ObjectAnimator
 import android.app.Activity
-import android.graphics.Color
+import android.graphics.*
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 val LOGTAG = "MainActivity"
 
 class MainActivity : Activity() {
-
     /** 生成処理 */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // リスナー登録
         setListener()
+
+        imageView.setImageBitmap(createTextToBitmap("テスト"))
     }
 
     /** リスナー登録 */
     fun setListener() {
-        input_reset.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_reset)) }
+        input_reset.setOnClickListener {
+            Log.d(LOGTAG, resources.getString(R.string.str_reset))
+            val anim = ObjectAnimator.ofFloat(imageView, "rotationY", 0f, 360f)
+            anim.repeatCount = 1
+            anim.duration = 100
+            anim.start()
+        }
         input_sharp.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_sharp)) }
         input_flat.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_flat)) }
         input_minor.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_minor)) }
@@ -52,5 +64,37 @@ class MainActivity : Activity() {
         input_sharp11.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_sharp11)) }
         input_13.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_13)) }
         input_flat13.setOnClickListener { Log.d(LOGTAG, resources.getString(R.string.str_flat13)) }
+    }
+
+    // テキストからBitmapを生成
+    fun createTextToBitmap(str : String): Bitmap {
+        val textView = TextView(this)
+        textView.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        textView.text = str
+        textView.typeface = Typeface.MONOSPACE
+
+        // TextViewにサイズを設定
+        textView.measure(
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED))
+        textView.layout(0, 0, textView.measuredWidth, textView.measuredHeight)
+
+        // Bitmapに変換
+        val bitmap = Bitmap.createBitmap(
+                textView.measuredWidth, textView.measuredHeight, Bitmap.Config.ARGB_8888 )
+        textView.draw(Canvas(bitmap))
+
+        Log.d(LOGTAG, "textView.measuredWidth:" + textView.measuredWidth + ", textView.measuredHeight:" + textView.measuredHeight)
+
+        return bitmap
+    }
+
+    fun getDisplayWidth(): Int {
+        return this.resources.displayMetrics.widthPixels
+    }
+
+    fun getDisplayHeight(): Int {
+        return this.resources.displayMetrics.heightPixels
     }
 }
